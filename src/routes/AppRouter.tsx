d6e0469ce64c7e13962ab253243f1988930f1437
eodevelop <eodevelop@naver.com>
2024-12-17 23:24:08 +0900

    포토폴리오 용이라 라우터 필요없을지 고민 미사용이 될경우 제거 예정

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 
 const AppRouter: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* 다른 라우트를 추가할 수 있습니다 */}
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+      </Router>
   );
 };
 
